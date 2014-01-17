6f4acc9105c7c5544042c40af0e65cd0210cff2a
Sami-Joonas Saarinen <sjsaarin@cs.helsinki.fi>
2014-01-17 14:31:22 +0200

    index.php luotu

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+  $tervehdys = "Ei mitään nähtävää täällä vielä..."; 
+?><!DOCTYPE HTML>
+<html>
+<head><title><?php echo $tervehdys; ?></title></head>
+<body>
+  <h1><?php echo $tervehdys; ?></h1>
+</body>
+</html>
